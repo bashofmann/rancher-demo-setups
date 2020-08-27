f0340f5a27b0ee2b5f9416aaae1e3b7e243dbc99
Bastian Hofmann <bashofmann@gmail.com>
2020-08-27 17:09:19 +0200

    Update k3s vms to ubuntu20

--- k3svms/data.tf
+++ k3svms/data.tf
@@ -3,14 +3,14 @@
 # AWS data
 # ----------------------------------------------------------
 
-# Use latest Ubuntu 18.04 AMI
+# Use latest Ubuntu 20.04 AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
   filter {
