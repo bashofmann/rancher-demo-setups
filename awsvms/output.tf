7af6fd62d01a1d0d276f46ae5988c6e71f211546
Bastian Hofmann <bashofmann@gmail.com>
2020-10-29 16:51:29 +0100

    Remove unnecessary elb from awsvms module

--- awsvms/output.tf
+++ awsvms/output.tf
@@ -5,7 +5,3 @@ output "rancher_cluster_node_ips" {
 output "downstream_cluster_node_ips" {
   value = [aws_instance.downstream-cluster.*.public_ip]
 }
-
-output "lb_ip" {
-  value = aws_elb.rancher-server-lb.dns_name
-}
