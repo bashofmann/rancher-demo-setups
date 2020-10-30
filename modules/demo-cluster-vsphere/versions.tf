c2535074acff744d1a4728e434d51177502a271e
Bastian Hofmann <bashofmann@gmail.com>
2020-10-30 10:24:17 +0100

    update rancher tf provider

--- modules/demo-cluster-vsphere/versions.tf
+++ modules/demo-cluster-vsphere/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     rancher2 = {
       source  = "rancher/rancher2"
-      version = "1.10.3"
+      version = "1.10.4"
     }
   }
   required_version = ">= 0.13"
--- modules/rancher-server/versions.tf
+++ modules/rancher-server/versions.tf
@@ -5,7 +5,7 @@ terraform {
     }
     rancher2 = {
       source  = "rancher/rancher2"
-      version = "1.10.3"
+      version = "1.10.4"
     }
   }
   required_version = ">= 0.13"
