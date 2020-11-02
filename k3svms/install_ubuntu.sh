18aa6ddeabb31be3c4bf95861ace86431e5e7f6b
Bastian Hofmann <bashofmann@gmail.com>
2020-11-02 12:23:15 +0100

    Change skript to k3sup

--- k3svms/install_ubuntu.sh
+++ k3svms/install_ubuntu.sh
@@ -4,12 +4,27 @@ set -e
 
 $(terraform output -state=terraform.tfstate -json node_ips | jq -r 'keys[] as $k | "export IP\($k)=\(.[$k])"')
 
-ssh -o StrictHostKeyChecking=no ubuntu@$IP0 "curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=v1.19 INSTALL_K3S_EXEC='server' K3S_TOKEN=mysecret K3S_KUBECONFIG_MODE=644 K3S_CLUSTER_INIT=1 sh -"
-ssh -o StrictHostKeyChecking=no ubuntu@$IP1 "curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=v1.19 INSTALL_K3S_EXEC='server' K3S_TOKEN=mysecret K3S_URL=https://${IP0}:6443 sh - "
-ssh -o StrictHostKeyChecking=no ubuntu@$IP2 "curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=v1.19 INSTALL_K3S_EXEC='server' K3S_TOKEN=mysecret K3S_URL=https://${IP0}:6443 sh - "
+k3sup install \
+  --ip $IP0 \
+  --user root \
+  --cluster \
+  --k3s-channel latest
 
-scp -o StrictHostKeyChecking=no ubuntu@$IP0:/etc/rancher/k3s/k3s.yaml kubeconfig
-sed -i "s/127.0.0.1/${IP0}/g" kubeconfig
+k3sup join \
+  --ip $IP1 \
+  --user root \
+  --server-user root \
+  --server-ip $IP0 \
+  --server \
+  --k3s-channel latest
+
+k3sup join \
+  --ip $IP2 \
+  --user root \
+  --server-user root \
+  --server-ip $IP0 \
+  --server \
+  --k3s-channel latest
 
 export KUBECONFIG=kubeconfig
 
