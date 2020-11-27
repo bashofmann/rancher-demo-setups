c86adbeb2c2878093353361a83d1d2b423420b8a
Bastian Hofmann <bashofmann@gmail.com>
2020-11-27 11:11:15 +0100

    Cleanup

--- vspherevms/provider.tf
+++ vspherevms/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
 provider "vsphere" {
   user                 = var.vcenter_user
   password             = var.vcenter_password
--- vspherevms/versions.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    vsphere = {
-      source = "hashicorp/vsphere"
-    }
-  }
-  required_version = ">= 0.13"
-}
