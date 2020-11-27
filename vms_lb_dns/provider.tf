fc8e10ea102adf7af9c1808dd1c9c08d0eb93e80
Bastian Hofmann <bashofmann@gmail.com>
2020-11-27 11:11:10 +0100

    Cleanup

--- vms_lb_dns/provider.tf
+++ vms_lb_dns/provider.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
