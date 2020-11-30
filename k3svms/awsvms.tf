1e1affbf377678b206ad245d349d1880a96dbd41
Bastian Hofmann <bashofmann@gmail.com>
2020-11-30 13:42:43 +0100

    Add provisioner

--- k3svms/awsvms.tf
+++ k3svms/awsvms.tf
@@ -1,13 +1,8 @@
-# AWS infrastructure resources
-
-# Temporary key pair used for SSH accesss
 resource "aws_key_pair" "quickstart_key_pair" {
   key_name_prefix = "${var.prefix}-k3s-"
   public_key      = file("${var.ssh_key_file_name}.pub")
 }
 
-// TODO more
-# Security group to allow all traffic
 resource "aws_security_group" "rancher_sg_allowall" {
   name        = "${var.prefix}-k3s-allowall"
   description = "Rancher quickstart - allow all traffic"
@@ -37,6 +32,19 @@ resource "aws_instance" "k3s" {
 
   root_block_device {
     volume_size = 80
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cloud-init status --wait"
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file(var.ssh_key_file_name)
+    }
   }
 
   tags = {
