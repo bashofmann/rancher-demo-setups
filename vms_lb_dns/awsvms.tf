cafa0e11d7a3cfa4fed3f32bdddf1940bc0825d1
Bastian Hofmann <bashofmann@gmail.com>
2020-12-10 17:00:05 +0100

    Also open 30001

--- vms_lb_dns/awsvms.tf
+++ vms_lb_dns/awsvms.tf
@@ -67,6 +67,12 @@ resource "aws_security_group" "rancher" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    from_port   = 30001
+    to_port     = 30001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 6443
     to_port     = 6443
     protocol    = "tcp"
