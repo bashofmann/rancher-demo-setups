3d9a4286d936cb5ab04cac04d57567c90d308115
Bastian Hofmann <bashofmann@gmail.com>
2020-12-17 14:02:43 +0100

    fix render script

--- vms_lb_dns/echo_install_ubuntu.sh
+++ vms_lb_dns/echo_install_ubuntu.sh
@@ -3,5 +3,6 @@
 set -e
 
 $(terraform output -state=terraform.tfstate -json node_ips | jq -r 'keys[] as $k | "export IP\($k)=\(.[$k])"')
+$(terraform output -state=terraform.tfstate -json "lb" | jq -r '"export LB=\(.)"')
 
 cat install_ubuntu.sh | envsubst > rendered_install.sh
