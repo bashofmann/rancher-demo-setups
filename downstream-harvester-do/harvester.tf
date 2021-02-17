219078c46ccc500745cc6a4015d2886203b61cf0
Bastian Hofmann <bashofmann@gmail.com>
2021-02-17 18:01:45 +0100

    Split harvester and longhorn installations

--- downstream-harvester-do/harvester.tf
+++ downstream-harvester-do/harvester.tf
@@ -6,9 +6,22 @@ resource "rancher2_catalog" "harvester" {
   cluster_id = rancher2_cluster_sync.harvester.id
 }
 
+resource "rancher2_catalog" "longhorn" {
+  name       = "longhorn"
+  url        = "https://charts.longhorn.io"
+  version    = "helm_v3"
+  scope      = "cluster"
+  cluster_id = rancher2_cluster_sync.harvester.id
+}
+
 resource "rancher2_project" "harvester" {
   name       = "harvester"
   cluster_id = rancher2_cluster.harvester.id
+}
+
+resource "rancher2_namespace" "longhorn" {
+  name       = "longhorn-system"
+  project_id = rancher2_project.harvester.id
 }
 
 resource "rancher2_namespace" "harvester" {
@@ -16,14 +29,23 @@ resource "rancher2_namespace" "harvester" {
   project_id = rancher2_project.harvester.id
 }
 
+resource "rancher2_app" "longhorn" {
+  catalog_name     = "${rancher2_cluster.harvester.id}:${rancher2_catalog.longhorn.name}"
+  name             = "longhorn-system"
+  project_id       = rancher2_project.harvester.id
+  template_name    = "longhorn"
+  template_version = "1.1.0"
+  target_namespace = rancher2_namespace.longhorn.id
+}
+
 resource "rancher2_app" "harvester" {
+  depends_on = [
+    rancher2_app.longhorn
+  ]
   catalog_name     = "${rancher2_cluster.harvester.id}:${rancher2_catalog.harvester.name}"
   name             = "harvester-system"
   project_id       = rancher2_project.harvester.id
   template_name    = "harvester"
   template_version = "0.1.0"
   target_namespace = rancher2_namespace.harvester.id
-  answers = {
-    "longhorn.enabled" = true
-  }
 }
