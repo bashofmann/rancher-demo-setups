f16ec7cfa333398ce836e585e1b21e95f50902d6
Bastian Hofmann <bashofmann@gmail.com>
2021-11-30 12:29:20 +0100

    Make downstream k3s standalone

--- downstream-k3s/demo-cluster.tf
+++ downstream-k3s/demo-cluster.tf
@@ -1,20 +1,20 @@
-resource "rancher2_cluster" "demo" {
-  name        = "${var.prefix}-demo"
-  description = "Cluster for demos"
-}
-
-resource "null_resource" "registration" {
-  depends_on = [
-    null_resource.k3s,
-    rancher2_cluster.demo
-  ]
-  provisioner "local-exec" {
-    command = rancher2_cluster.demo.cluster_registration_token[0].command
-    environment = {
-      KUBECONFIG = "${path.module}/kubeconfig"
-    }
-  }
-}
+//resource "rancher2_cluster" "demo" {
+//  name        = "${var.prefix}-demo"
+//  description = "Cluster for demos"
+//}
+//
+//resource "null_resource" "registration" {
+//  depends_on = [
+//    null_resource.k3s,
+//    rancher2_cluster.demo
+//  ]
+//  provisioner "local-exec" {
+//    command = rancher2_cluster.demo.cluster_registration_token[0].command
+//    environment = {
+//      KUBECONFIG = "${path.module}/kubeconfig"
+//    }
+//  }
+//}
 
 module "demo-workloads" {
   depends_on = [
--- downstream-k3s/provider.tf
+++ downstream-k3s/provider.tf
@@ -3,10 +3,10 @@ terraform {
     aws = {
       source = "hashicorp/aws"
     }
-    rancher2 = {
-      source  = "rancher/rancher2"
-      version = "1.10.6"
-    }
+    //    rancher2 = {
+    //      source  = "rancher/rancher2"
+    //      version = "1.10.6"
+    //    }
   }
   required_version = ">= 0.13"
 }
@@ -16,9 +16,9 @@ provider "aws" {
   secret_key = var.aws_secret_key
   region     = var.aws_region
 }
-provider "rancher2" {
-  api_url    = var.rancher_url
-  insecure   = true
-  access_key = var.rancher_access_key
-  secret_key = var.rancher_secret_key
-}
+//provider "rancher2" {
+//  api_url    = var.rancher_url
+//  insecure   = true
+//  access_key = var.rancher_access_key
+//  secret_key = var.rancher_secret_key
+//}
